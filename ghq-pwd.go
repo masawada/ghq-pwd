aba32e064119851a6d33f3c7ee8e662c0f3f3055
Masayoshi Wada <developer@andantesoftware.com>
2016-03-01 02:10:26 +0900

    now it works

---
+++ ghq-pwd.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+  "os"
+  "path/filepath"
+  "fmt"
+  "log"
+  "strings"
+  "regexp"
+
+  "github.com/codegangsta/cli"
+  "github.com/mitchellh/go-homedir"
+)
+
+func main() {
+  app := cli.NewApp()
+  app.Name = "ghq-pwd"
+  app.Usage = "shorten pwd path under the ghq."
+  app.Action = func(c *cli.Context) {
+    fmt.Println(ghqpwd())
+  }
+
+  app.Run(os.Args)
+}
+
+func ghqpwd() (string) {
+    // get pwd
+    fullPwd, pwdErr := filepath.Abs(filepath.Dir(os.Args[0]))
+    if pwdErr != nil {
+      log.Fatal(pwdErr)
+    }
+
+    // get $HOME
+    homeDir, homeDirErr := homedir.Dir()
+    if homeDirErr != nil {
+      log.Fatal(homeDirErr)
+    }
+
+    rep := regexp.MustCompile(`^~/(.go/src|.ghq)/.+?/`)
+
+    return rep.ReplaceAllString(strings.Replace(fullPwd, homeDir, "~", 1), "ghq:")
+}
